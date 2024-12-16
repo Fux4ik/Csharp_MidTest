10cdf747a63a41865397938b24f5a5b36b2bdcde
Fux4ik <fux4ik@gmail.com>
2024-12-16 12:21:33 +0200

    Csharp_MidTest

---
+++ ConsoleApp2/ConsoleApp2/MyFunctions.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace MidTest
+{
+    public static class MyFunctions
+    {
+        public static long Factorial(int n)
+        {
+            long result = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                result *= i;
+            }
+            return result;
+        }
+
+        public static double CosMethod(double x, double acc, out int counter)
+        {
+            counter = 0;
+
+            double radians = x;
+
+            double CosValue = 1;
+            int power = 2;
+            int sign = -1;
+            double term;
+            do
+            {
+                term = sign * Math.Pow(radians, power) / Factorial(power);
+
+
+                CosValue += term;
+
+
+                power += 2;
+                sign *= -1;
+
+
+
+                counter++;
+            }
+            while (Math.Abs(term) >= acc);
+            Console.WriteLine($"It was needed to have: {counter} amount of iterations");
+            return CosValue;
+        }
+
+
+
+
+        public static void SaveToFile(string name_of_file, double accuracy, double result, int counter)
+        {
+            try
+            {
+                File.WriteAllText(name_of_file,
+                    $"Chosen accuracy: {accuracy}\n" +
+                    $"Calculated cos(x): {result}\n" +
+                    $"Amount of iterations: {counter}"
+                    );
+                Console.WriteLine($"Results successfully written to file: {name_of_file}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error writing to file: {ex.Message}");
+            }
+        }
+
+        public static void ReadFile(string name_of_file)
+        {
+            try
+            {
+                if (File.Exists(name_of_file))
+                {
+                    string content = File.ReadAllText(name_of_file);
+                    Console.WriteLine("File content:");
+                    Console.WriteLine(content);
+                }
+                else
+                {
+                    Console.WriteLine($"File \"{name_of_file}\" does not exist.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error reading the file: {ex.Message}");
+            }
+        }
+    }
+}
+
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using System.Reflection;
+using MidTest;
+
+namespace MidTest
+{
+
+    namespace Kalashnyk
+    {
+
+
+
+
+        internal class Program
+        {
+
+
+            static void Main(string[] args)
+            {
+                string name_of_file;
+                int choice;
+
+                double accuracy;
+                Console.WriteLine("Choose the value for x: ");
+                double x = double.Parse(Console.ReadLine());
+                Console.WriteLine("Choose the accurancy from following: 1 - 10(-6), 2 - 10(-9), 3 - 10(-12) ");
+                while (true)
+                {
+                    int acc = int.Parse(Console.ReadLine());
+                    switch (acc)
+                    {
+                        case 1:
+                            accuracy = Math.Pow(10, -6);
+                            break;
+                        case 2:
+                            accuracy = Math.Pow(10, -9);
+                            break;
+                        case 3:
+                            accuracy = Math.Pow(10, -12);
+                            break;
+                        default:
+                            Console.WriteLine("Incorrect input, try again");
+                            continue;
+                    }
+                    break;
+                }
+
+                double result = MyFunctions.CosMethod(x, accuracy, out int counter);
+                Console.WriteLine($"The calculated value of cos({x}) is: {result}");
+                Console.WriteLine("This data will be transfered to txt file. Would you like to rename the file?");
+                Console.WriteLine("1 - Yes, 2 - No");
+               
+                while (true)
+                {
+                    choice = int.Parse(Console.ReadLine());
+                    switch (choice)
+                    {
+                        case 1:
+                            Console.WriteLine("How you would like to rename the file?");
+                            name_of_file = Console.ReadLine();
+                            break;
+                        case 2:
+                            Console.WriteLine("The name of file <result> saved on your computer");
+                            name_of_file = "result";
+                            break;
+                        default:
+                            Console.WriteLine("Incorrect input, please try again.");
+                            continue;
+                    }
+                    break;
+                }
+                MyFunctions.SaveToFile(name_of_file, accuracy, result, counter);
+                MyFunctions.ReadFile(name_of_file);
+
+                Console.WriteLine(Math.Cos(x));
+
+            }
+        }
+    }
+}
